f409b6b3b2056e8b14e4600c5b136787dfd0b1f5
Robin Richard-Canavaggio <22174078+r-richardcanavaggio@users.noreply.github.com>
2026-09-10 18:09:55 +0200

    Regression computes successfully, need to organize graph and compute algorithm precision

--- compute_regression.py
+++ compute_regression.py
@@ -1,6 +1,8 @@
 import sys
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import json
 from estimate_price import estimate_price
 
 
@@ -37,25 +39,43 @@ def main():
     eps = 1e-12
 
     mileage = df['km'].to_numpy()
-    mileage = (mileage - mileage.min()) / (mileage.max() - mileage.min())
     price = df['price'].to_numpy()
+
+    km_min = mileage.min()
+    km_max = mileage.max()
+    norm_mile = (mileage - km_min) / (km_max - km_min)
     m = len(df)
 
     while True:
         old0 = theta_0
         old1 = theta_1
-        estimation = estimate_price(mileage, theta_0, theta_1)
+        estimation = estimate_price(norm_mile, theta_0, theta_1)
         tmp_0 = learning_rate * np.sum(estimation - price) / m
-        tmp_1 = learning_rate * (np.sum((estimation - price) * mileage) / m)
+        tmp_1 = learning_rate * (np.sum((estimation - price) * norm_mile) / m)
         theta_0 = theta_0 - tmp_0
         theta_1 = theta_1 - tmp_1
-        print(f"Theta 0: {theta_0:.15f} | Theta 1: {theta_1:.15f}", end='\r', flush=True)
+        print(f"Theta 0: {theta_0:.15f} | Theta 1: {theta_1:.15f}",
+              end='\r',
+              flush=True)
         if abs(old0 - theta_0) < eps and abs(old1 - theta_1) < eps:
             break
 
-    theta_0 = theta_0 * (mileage.max() - mileage.min()) + mileage.min()
-    theta_1 = theta_1 * (mileage.max() - mileage.min()) + mileage.min()
-    print(f"\nDenormalized\nTheta 0: {theta_0} | Theta 1: {theta_1}")
+    theta_1 = theta_1 / (km_max - km_min)
+    theta_0 = theta_0 - (theta_1 * km_min)
+
+    export = {
+        "theta_0": theta_0,
+        "theta_1": theta_1
+    }
+
+    with open("model.json", "w") as fichier:
+        json.dump(export, fichier, indent=4)
+
+    fig, ax = plt.subplots()
+    ax.scatter(mileage, price, c='blue')
+    ax.plot(mileage, estimate_price(mileage, theta_0, theta_1), color='red')
+
+    plt.show()
 
 
 if __name__ == "__main__":
--- estimate_price.py
+++ estimate_price.py
@@ -1,16 +1,27 @@
 import sys
+import json
+
 
 def estimate_price(mileage: int, theta_0: float = 0.0, theta_1: float = 0.0) -> float:
     return theta_0 + (theta_1 * mileage)
 
 
 def main():
-    if len(sys.argv) != 1:
-        return 1
-        
-    print("Enter mileage: ")
-    line = sys.stdin.read()
+    try:
+        with open("model.json", "r") as fichier:
+            parametres = json.load(fichier)
 
+        theta_0 = parametres["theta_0"]
+        theta_1 = parametres["theta_1"]
+    except FileNotFoundError:
+        print("Fichier model.json introuvable, lancement avec valeur par defaut (0,0)")
+        theta_0 = 0.0
+        theta_1 = 0.0
+
+    mileage = input("Enter mileage: ")
+    mileage = int(mileage)
+    estimation = estimate_price(mileage, theta_0, theta_1)
+    print(f"Estimated price for given mileage is: {estimation}")
 
 
 if __name__ == "__main__":
